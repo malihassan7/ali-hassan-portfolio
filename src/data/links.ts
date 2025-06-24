8d5735424760ee29bd731f35f962fc546d556f84
muhammadaliht <muhammadali38906@gmail.com>
2025-06-24 23:19:14 +0500

    update link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,5 @@
 export const Links = {
-    github: "https://github.com/aathifzahir",
+    github: "https://github.com/muhammadaliht",
     linkedin: "https://www.linkedin.com/in/ali-hassan-ah7",
     email: "mailto:muhammadali38906@gmail.com",
     resume: "https://www.dropbox.com/scl/fi/n8ae7ee63lwf6hkmz5la4/ALI_HASSAN-Full-Stack-Web-Developer.pdf?rlkey=73qbl6g409b9wwn06qrgjui1s&st=m0hpg9ea&dl=1",
