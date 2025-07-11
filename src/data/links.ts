a0307be543e13f2280f019cd1db875c71b524b2a
muhammadaliht <muhammadali38906@gmail.com>
2025-07-11 20:12:52 +0500

    update resume

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const Links = {
     github: "https://github.com/muhammadaliht",
     linkedin: "https://www.linkedin.com/in/ali-hassan-ah7",
     email: "mailto:muhammadali38906@gmail.com",
-    resume: "https://www.dropbox.com/scl/fi/n8ae7ee63lwf6hkmz5la4/ALI_HASSAN-Full-Stack-Web-Developer.pdf?rlkey=73qbl6g409b9wwn06qrgjui1s&st=m0hpg9ea&dl=1",
+    resume: "https://www.dropbox.com/scl/fi/gvukktj4dqe1e1l7usbrt/Ali-Hassan-Full-Stack-Web-Developer.pdf?rlkey=okuykbjqo3xy59yii80azkiti&st=o0zsycma&dl=1",
     emailonly: "muhammadali38906@gmail.com",
     location: "Sargodha, Pakistan",
     phone: "+92 312 6038906",
